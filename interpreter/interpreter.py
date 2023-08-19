89d0c563413e14151e2f0f13752c2db88ae20cba
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-19 14:00:56 +0000

    Sat, Aug 19, 2023, 9:00 PM +07:00

--- interpreter/interpreter.py
+++ interpreter/interpreter.py
@@ -1,6 +1,5 @@
 s = input("Expression: ")
-result = round(eval(s),1)
-if (result==eval(s)):
-    print(f"{result}.0")
-else:
-    print(result)
+result = str(round(eval(s),1))
+print(result, end="")
+if "." not in result:
+    print(".0")

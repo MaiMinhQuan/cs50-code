e8d6c83c741b96cdb107c1131d2c9678da15492b
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-19 13:34:14 +0000

    Sat, Aug 19, 2023, 8:34 PM +07:00

---
+++ extensions/extensions.py
@@ -0,0 +1,16 @@
+s = input("File nam: ")
+s = s.lower()
+if ".gif" in s:
+    print("image/gif")
+elif ".png" in s:
+    print("image/png")
+elif ".jgp" or ".jpeg" in s:
+    print("image/jpeg")
+elif ".pdf" in s:
+    print("application/pdf")
+elif ".zip" in s:
+    print("application/zip")
+elif ".txt" in s:
+    print("text/plain")
+else:
+    print("application/octet-stream")

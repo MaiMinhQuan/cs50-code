b5d7ce3117e9d49f251555b9d2a9c556746cc314
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-19 12:31:22 +0000

    Sat, Aug 19, 2023, 7:31 PM +07:00

--- deep/deep.py
+++ deep/deep.py
@@ -1,3 +1,6 @@
-s = input()
+s = input("What is the Answer to the Great Question of Life, the Universe, and Everything? ")
 
-if s=="42" or s=="forty-two" or s==
+if s=="42" or s=="forty-two" or s=="forty two":
+    print("Yes")
+else:
+    print("No")

249b399d9ef3a9763279a35efb1b6f870cb9037e
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-20 13:37:06 +0000

    Sun, Aug 20, 2023, 8:37 PM +07:00

--- twttr/twttr.py
+++ twttr/twttr.py
@@ -1,6 +1,6 @@
 s = input("Input: ")
 print("Output: ", end = "")
 for i in s:
-    if i not in ["u", "e", "o", "a", "i"]:
+    if i not in ["u", "e", "o", "a", "i", "U", "E", "O", "A", "I"]:
         print(i, end = "")
 print()

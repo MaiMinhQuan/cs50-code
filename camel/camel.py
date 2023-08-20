eddfa8cb082bf017ee9bdf599f24f725a038a09a
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-20 13:00:10 +0000

    Sun, Aug 20, 2023, 8:00 PM +07:00

--- camel/camel.py
+++ camel/camel.py
@@ -2,6 +2,6 @@ s = input("camelCase: ")
 
 for i in s:
     if i.isupper()==True:
-        print(f"_{i.tolower()}", end = "")
+        print(f"_{i.lower()}", end = "")
     else:
         print(i, end = "")

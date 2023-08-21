beb9907408ad64ca55f1c8af66c28e693d4420b1
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-21 03:26:48 +0000

    Mon, Aug 21, 2023, 10:26 AM +07:00

--- fuel/fuel.py
+++ fuel/fuel.py
@@ -1,6 +1,6 @@
 while True:
     s = input("Fraction: ")
-    x,y = s.split(" ")
+    x,y = s.split("/")
     try:
         x=int(x)
         y=int(y)
@@ -11,3 +11,9 @@ while True:
         continue
 
 r = x/y*100
+if r>=99:
+    print("F")
+elif r<=1:
+    print("E")
+else:
+    print(f"{round(r)}%")

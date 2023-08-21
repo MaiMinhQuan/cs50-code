05cf32f87d5c10e5b9fa6e112c95c0808a764324
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-21 05:08:23 +0000

    Mon, Aug 21, 2023, 12:08 PM +07:00

---
+++ outdated/outdated.py
@@ -0,0 +1,14 @@
+a = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
+
+while True:
+    s = input("Date: ")
+    if "/" in s:
+        x,y,z = s.split("/")
+    elif "," in s:
+        s = s.replace(",", "")
+        x,y,z = s.split(" ")
+        if x in a:
+            x = a.index(x)+1
+            break
+    else:
+        continue

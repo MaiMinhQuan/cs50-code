f5096de9bf6db600f1da6acd8b2b3ca7ea539288
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-21 05:25:32 +0000

    Mon, Aug 21, 2023, 12:25 PM +07:00

--- outdated/outdated.py
+++ outdated/outdated.py
@@ -2,22 +2,24 @@ a = ["January", "February", "March", "April", "May", "June", "July", "August", "
 
 while True:
     s = input("Date: ")
-    if "/" in s:
-        x,y,z = map(int, s.split("/"))
-        if y>31 or x>12:
-            continue
-        break
-    elif "," in s:
-        s = s.replace(",", "")
-        x,y,z = s.split(" ")
-        if x in a:
-            x = a.index(x)+1
-            y = int(y)
-            z = int(z)
-            if y>31:
+    try:
+        if "/" in s:
+            x,y,z = map(int, s.split("/"))
+            if y>31 or x>12:
                 continue
             break
-    else:
+        elif "," in s:
+            s = s.replace(",", "")
+            x,y,z = s.split(" ")
+            if x in a:
+                x = a.index(x)+1
+                y = int(y)
+                z = int(z)
+                if y>31:
+                    continue
+                break
+        else:
+            continue
+    except ValueError:
         continue
-
 print(f"{z}-{x:02}-{y:02}")

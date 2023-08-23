2a1fb31cc14ffd1137be6d4342133056eb809fef
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-23 16:11:44 +0000

    Wed, Aug 23, 2023, 11:11 PM +07:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,6 +1,26 @@
 import random
 
 def main():
+    for i in range(10):
+        level = get_level()
+        x = generate_integer(level)
+        y = generate_integer(level)
+        result = x + y
+        count = 0
+        while count<4:
+            try:
+                print(f"{x} + {y} = ", end="")
+                ans = int(input())
+                count+=1
+                if ans == result:
+                    break
+                else:
+                    print("EEE")
+            except:
+                print("EEE")
+        if count == 3:
+            print(f"{x} + {y} = {result}")
+
 
 def get_level():
     while True:
@@ -14,4 +34,9 @@ def get_level():
             continue
 
 def generrate_integer(level):
-    
+    if level == 1:
+        return random.randint(0, 9)
+    elif level == 2:
+        return random.randint(10, 99)
+    else:
+        return random.randint(100, 999)

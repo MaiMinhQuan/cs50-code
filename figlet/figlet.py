e95feb995a8a901cf96b939bf6caacfe45cb9a00
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-23 14:57:56 +0000

    Wed, Aug 23, 2023, 9:57 PM +07:00

--- figlet/figlet.py
+++ figlet/figlet.py
@@ -8,7 +8,7 @@ f = figlet.getFonts()
 
 if len(sys.argv) == 1:
     figlet.setFont(font=random.choice(f))
-elif len(sys.argv) == 2 and (sys.argv[1] == "f" or sys.argv[1] == "--font") and sys.argv[2] in f:
+elif len(sys.argv) == 3 and (sys.argv[1] == "f" or sys.argv[1] == "--font") and sys.argv[2] in f:
     figlet.setFont(font=sys.argv[2])
 else:
     sys.exit("Invalid usage")

848d928c73ace98b77d81b1ec716bd032a98c886
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-23 16:01:39 +0000

    Wed, Aug 23, 2023, 11:01 PM +07:00

--- professor/professor.py
+++ professor/professor.py
@@ -1,4 +1,17 @@
 import random
 
 def main():
+
+def get_level():
+    while True:
+        try:
+            level = int(input("Level: "))
+            if level in [1, 2, 3]:
+                return level
+            else:
+                continue
+        except:
+            continue
+
+def generrate_integer(level):
     

0aba2a1185758350cdede320c678417e99393ad4
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-24 05:24:35 +0000

    Thu, Aug 24, 2023, 12:24 PM +07:00

--- test_fuel/fuel.py
+++ test_fuel/fuel.py
@@ -1,7 +1,30 @@
 def main():
     i = input("Fraction: ")
     value = convert(i)
-
+    print(gauge(i))
 def convert(i):
     while True:
-        
+        try:
+            ts, ms = i.split("/")
+            ts = int(ts)
+            ms = int(ms)
+            f = ts/ms
+            if f<=1:
+                f*=100
+                return f
+            else:
+                i = input("Fraction: ")
+                pass
+        except (ValueError, ZeroDivisionError):
+            raise
+
+def gauge(i):
+    if i <= 1:
+        return "E"
+    elif i >= 99:
+        return "F"
+    else:
+        return f"{i}%"
+
+if __name__ == "__main__":
+    main()

c912e92e823edf7469b840f40398e7fd4f3a7ebc
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-24 04:38:01 +0000

    Thu, Aug 24, 2023, 11:38 AM +07:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -6,7 +6,8 @@ def main():
         print("Invalid")
 
 def is_valid(s):
-    if len(s)<2 or len(s)>6 return False
+    if len(s)<2 or len(s)>6:
+        return False
     if s[0].isalpha() == False or s[1].isalpha() == False:
         return False
     for i in range(len(s)-1):

b3d9fed122d4a73658ecb66a06aebc15c41cf2bf
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-24 04:34:47 +0000

    Thu, Aug 24, 2023, 11:34 AM +07:00

--- test_plates/plates.py
+++ test_plates/plates.py
@@ -9,10 +9,11 @@ def is_valid(s):
     if len(s)<2 or len(s)>6 return False
     if s[0].isalpha() == False or s[1].isalpha() == False:
         return False
-    for i in range(len(s)):
+    for i in range(len(s)-1):
         if s[i].isdigit() == True and s[i+1].isalpha() == True:
             return False
         if s[i].isalpha() == True and s[i+1] == "0":
             return False
-        if s[i].isalnum() == False:
-            return False
+    for i in s:
+        if i.isalnum() == False:
+            return

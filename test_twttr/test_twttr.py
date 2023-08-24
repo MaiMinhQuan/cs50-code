bb6bb4a19a08c58ffbd37716a11652150d0c7f6b
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-24 03:48:19 +0000

    Thu, Aug 24, 2023, 10:48 AM +07:00

--- test_twttr/test_twttr.py
+++ test_twttr/test_twttr.py
@@ -1,3 +1,22 @@
 from twttr import shorten
 
-def main()
+def main():
+    test_na()
+    test_pa()
+    test_dau()
+
+def test_na():
+    assert shorten("UUUUa") == ""
+    assert shorten("ATCScsd") == "TCScsd"
+    assert shorten("Twitter") == "Twttr"
+
+def test_pa():
+    assert shorten("klmnbg") == "klmnbg"
+    assert shorten("qwrty") == "qwrty"
+
+def test_dau():
+    assert shorten(";.") == ";."
+    assert shorten(",/") == ",/"
+
+if __name__ == "__main__":
+    main()

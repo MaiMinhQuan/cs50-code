774aa0d0a1183e82b11b06f37daad3b6d0dbbe65
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-24 04:46:44 +0000

    Thu, Aug 24, 2023, 11:46 AM +07:00

--- test_plates/test_plates.py
+++ test_plates/test_plates.py
@@ -3,4 +3,21 @@ from plates import is_valid
 def main():
 
 def test_2start():
-    assert("")
+    assert is_valid("AA563") == True
+    assert is_valid("A563") == False
+
+def test_len():
+    assert is_valid("M") == False
+    assert is_valid("A498662") == False
+
+def test_numal():
+    assert is_valid("AA5O") == False
+
+def test_al0():
+    assert is_valid("VD0") == False
+
+def test_dau():
+    assert is_valid("AS<>") == False
+
+if __name__ == "__main__":
+    main()

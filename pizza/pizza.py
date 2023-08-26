59fabbbebb32194713422964325788eed093ce49
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-26 04:38:06 +0000

    Sat, Aug 26, 2023, 11:38 AM +07:00

---
+++ pizza/pizza.py
@@ -0,0 +1,28 @@
+import sys
+import tabulate
+import csv
+
+def main():
+    check_input()
+    pizza = []
+    try:
+        with open(sys.argv[1]) as file:
+            reader = csv.reader(file)
+            for row in reader:
+                pizza.append(row)
+    except:
+        sys.exit("File does not exit")
+
+    print(tabulate(pizza[1:], headers = pizza[0], tablefmt = "grid"))
+
+
+def check_input():
+    if len(sys.argv) < 2:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 2:
+        sys.exit("Too many command-line arguments")
+    if ".csv" not in sys.argv[1]:
+        sys.exit("Not a CSV file")
+
+if __name__ == "__main__":
+    main()

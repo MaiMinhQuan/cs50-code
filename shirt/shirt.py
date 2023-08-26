f4a292c1350361e50fd10713e48732f3ea79a3a7
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-26 06:03:56 +0000

    Sat, Aug 26, 2023, 1:03 PM +07:00

--- shirt/shirt.py
+++ shirt/shirt.py
@@ -1,1 +1,25 @@
 import sys
+from os.path import splitext
+from PIL import Image, ImageOps
+
+def main():
+
+def check_input():
+    if len(sys.argv) < 3:
+        sys.exit("Too few command-line arguments")
+    if len(sys.argv) > 3:
+        sys.exit("Too many command-line arfuments")
+    file1 = sys.argv[1].splitext()
+    file2 = sys.argv[2].splitext()
+    d1 = file1[1]
+    d2 = file2[1]
+    if check_duoi(d1) == False or check_duoi(d2) == False:
+        sys.exit("Invalid input")
+    if d1 != d2:
+        sys.exit("Input and output have different extensions")
+        
+
+def check_duoi(duoi):
+    if duoi in [".jpg", ".jpeg", ".png"]:
+        return True
+    return False

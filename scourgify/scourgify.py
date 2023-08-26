548cf90ab9312ffddc5da71bd04fd5376ceb0eb2
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-26 05:23:19 +0000

    Sat, Aug 26, 2023, 12:23 PM +07:00

--- scourgify/scourgify.py
+++ scourgify/scourgify.py
@@ -3,17 +3,21 @@ import csv
 
 def main():
     check_input()
-    after = []
+    tmp = []
     try:
         with open(sys.argv[1]) as before:
             reader = csv.DictReader(before)
             for row in reader:
                 last, first = row["name"].split(", ")
-                after.append({"first" : first, "last" : last, "house" : row["house"]})
+                tmp.append({"first" : first, "last" : last, "house" : row["house"]})
     except:
         sys.exit(f"Could not read {sys.argv[1]}")
 
-    with open("")
+    with open(sys.argv[2], "w") as after:
+        writer = csv.DictWriter(after, fieldnames = ["first", "last", "house"] )
+        writer.writerow({"first" : "first", "last" : "last", "house" : "house"})
+        for row in tmp:
+            writer.writerow({"first" : row["first"], "last" : row["last"], "house" : row["house"]})
 
 
 def check_input():

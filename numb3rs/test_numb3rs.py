ecfdf1bbe4464e08344b6d62788646862a1d376c
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 16:14:36 +0000

    Mon, Aug 28, 2023, 11:14 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,10 +5,10 @@ def main():
     test_khl()
 
 def test_hl():
-    assert validate(r"1.2.3.4") == True
-    assert validate(r"1.2.3") == False
-    assert validate(r"1.2") == False
-    assert validate(r"1") ==False
+    assert validate(r"12.26.35.84") == True
+    assert validate(r"48.62.49") == False
+    assert validate(r"99.66") == False
+    assert validate(r"8") ==False
 
 def test_khl():
     assert validate(r"100.100.100.100") == True

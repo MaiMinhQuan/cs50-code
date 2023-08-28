13f66f58d312c3210d8c25a0fe1fa4153812a0eb
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 15:06:38 +0000

    Mon, Aug 28, 2023, 10:06 PM +07:00

--- working/working.py
+++ working/working.py
@@ -5,8 +5,7 @@ def main():
     print(convert(input("Hours: ")))
 
 def convert(s):
-    if match = re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s)
-    if match:
+    if match := re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s):
         a = match.groups()
         if int(a[0]) > 12 or int(a[3]) > 12:
             raise ValueError

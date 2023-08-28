ffabf2a454d58e4f845787c248d6a019c6701b64
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 14:52:28 +0000

    Mon, Aug 28, 2023, 9:52 PM +07:00

--- working/working.py
+++ working/working.py
@@ -8,7 +8,12 @@ def convert(s):
     match = re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s)
     if match:
         a = match.groups()
+        if int(a[0]) > 12 or int(a[3]) > 12:
+            raise ValueError
+        if int(a[1]) >= 60 or int(a[4]) >= 60:
+            raise ValueError
         return a
+
     else:
         raise ValueError
 

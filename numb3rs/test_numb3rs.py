f091c55e9486a39bafb645f752bf279b688b30e7
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 16:07:22 +0000

    Mon, Aug 28, 2023, 11:07 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -5,18 +5,18 @@ def main():
     test_khl()
 
 def test_hl():
-    assert validate("1.2.3.4") == True
-    assert validate("50.26.255.100") == True
-    assert validate("15.56.89.13") == True
+    assert validate(r"1.2.3.4") == True
+    assert validate(r"50.26.255.100") == True
+    assert validate(r"15.56.89.13") == True
 
 def test_khl():
-    assert validate("256.1.5.59") == False
-    assert validate("1000.6.9.213") == False
-    assert validate("cat") == False
-    assert validate("1") == False
-    assert validate("9.8.56") == False
-    assert validate("5.6") == False
-    assert validate("45.1568") == False
+    assert validate(r"256.1.5.59") == False
+    assert validate(r"1000.6.9.213") == False
+    assert validate(r"cat") == False
+    assert validate(r"1") == False
+    assert validate(r"2.3.4") == False
+    assert validate(r"1.2") == False
+    assert validate(r"45.56") == False
 
 
 if __name__ == "__main__":

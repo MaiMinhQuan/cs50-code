44a1128d9cb5984ffd320ce31d002ab9ed45881a
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 16:11:34 +0000

    Mon, Aug 28, 2023, 11:11 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -8,15 +8,14 @@ def test_hl():
     assert validate(r"1.2.3.4") == True
     assert validate(r"1.2.3") == False
     assert validate(r"1.2") == False
-    assert validate(r"cat") ==False
+    assert validate(r"1") ==False
 
 def test_khl():
-    assert validate(r"256.1.5.59") == False
+    assert validate(r"100.100.100.100") == True
     assert validate(r"1000.6.9.213") == False
     assert validate(r"100.5641.26.59") == False
     assert validate(r"1.2.3.1597") == False
     assert validate(r"45.16.7895.23") == False
-    assert validate(r"45.56") == False
 
 
 if __name__ == "__main__":

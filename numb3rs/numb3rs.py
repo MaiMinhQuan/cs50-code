4b4f082636556f571756b514974bd41f2f4e441b
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 11:35:35 +0000

    Mon, Aug 28, 2023, 6:35 PM +07:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -4,6 +4,13 @@ import sys
 def main():
     print(validate(input("IPv4 Address: ")))
 
-def validate(ip):
+"""def validate(ip):
     try:
-        matches = re.search("(.+)\.(.+)\.(.+)\.(.+)", ip)
+        if matches := re.search("^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip):
+            a = int(matches.group(1))
+            b = int(matches.group(2))
+            c = int(matches.group(3))
+            d = int(matches.group(4))
+            if
+
+

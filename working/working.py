cf1864f9049df3cf8efd8262c43d0cec5fbac3e5
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 15:00:11 +0000

    Mon, Aug 28, 2023, 10:00 PM +07:00

--- working/working.py
+++ working/working.py
@@ -12,10 +12,29 @@ def convert(s):
             raise ValueError
         if int(a[1]) >= 60 or int(a[4]) >= 60:
             raise ValueError
-        return a
+        first = chuan_hoa(a[0], a[1], a[2])
+        last = chuan_hoa(a[3], a[4], a[5])
+        return first
 
     else:
         raise ValueError
+
+def chuan_hoa(g, p, sc):
+    if sc == "PM":
+        if int(g) == 12:
+            g = 12
+        else:
+            g = int(g) + 12
+    else:
+        if int(g) == 12:
+            g = 0
+        else:
+            g = int(g)
+
+    if p == None:
+        p = "00"
+
+    return f"{g:02}:{p} {sc}"
 
 if __name__ == "__main__":
     main()

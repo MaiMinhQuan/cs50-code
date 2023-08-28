c746036d0e6b6d7da01b76831e01cc35775a53ec
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 15:05:39 +0000

    Mon, Aug 28, 2023, 10:05 PM +07:00

--- working/working.py
+++ working/working.py
@@ -5,15 +5,13 @@ def main():
     print(convert(input("Hours: ")))
 
 def convert(s):
-    match = re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s)
+    if match = re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s)
     if match:
         a = match.groups()
         if int(a[0]) > 12 or int(a[3]) > 12:
             raise ValueError
 
-        first = chuan_hoa(a[0], a[1], a[2])
-        last = chuan_hoa(a[3], a[4], a[5])
-        return f"{first} {last}"
+        return f"{chuan_hoa(a[0], a[1], a[2])} to {chuan_hoa(a[3], a[4], a[5])}"
 
     else:
         raise ValueError

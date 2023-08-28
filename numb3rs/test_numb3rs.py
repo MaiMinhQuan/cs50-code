336251ae104767b7cc016556715242dcf6988df3
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 12:34:43 +0000

    Mon, Aug 28, 2023, 7:34 PM +07:00

--- numb3rs/test_numb3rs.py
+++ numb3rs/test_numb3rs.py
@@ -2,21 +2,22 @@ from numb3rs import  validate
 
 def main():
     test_hl()
-    test_khls()
-    test_khlc()
+    test_khl()
 
 def test_hl():
     assert validate("1.2.3.4") == True
     assert validate("50.26.255.100") == True
+    assert validate("15.56.89.13") == True
 
-def test_khls():
+def test_khl():
     assert validate("256.1.5.59") == False
     assert validate("1000.6.9.213") == False
-
-def test_khlc():
     assert validate("cat") == False
-    assert validate("1.2.3.5.6") == False
     assert validate("1") == False
+    assert validate("9.8.56") == False
+    assert validate("5.6") == False
+    assert validate("45.1568") == False
+
 
 if __name__ == "__main__":
     main()

f3d5379d89b2403a5446a1c352a7a05333b5a9ab
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 13:59:27 +0000

    Mon, Aug 28, 2023, 8:59 PM +07:00

--- watch/watch.py
+++ watch/watch.py
@@ -5,7 +5,7 @@ def main():
 
 def parse(s):
     if re.search(r"<iframe(.)*><\/iframe>", s):
-        if matches := re.search(r"(http(s)*:\/\/(www\.)*youtube\.com/embed\/)([a-z_A-Z_0-9]+)", s):
+        pattern = re.search(r"(http(s)*:\/\/(www\.)*youtube\.com/embed\/)([a-z_A-Z_0-9]+)", s):
             url = matches.group
             return "https://youtu.be/" + url[3]
 

8e0ae4b606f8d3de1956c2db718fa377533f1314
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 15:18:36 +0000

    Mon, Aug 28, 2023, 10:18 PM +07:00

--- working/test_working.py
+++ working/test_working.py
@@ -1,5 +1,29 @@
 from working import convert
 
 def main():
+    test_hl()
+    test_saiform()
+    test_saigio()
+    test_saiphut()
 
-def 
+def test_hl():
+    assert convert("9 AM to 5 PM") == "09:00 to 17:00"
+    assert convert("7:19 AM to 1:27 PM") == "7:19 to 13:27"
+
+def test_saiform():
+    with pytest.raises(ValueError):
+        convert("7 AM - 6PM")
+        convert("9:30 AM : 5:19 PM")
+
+def test_saigio():
+    with pytest.raises(ValueError):
+        convert("13 AM to 5 PM")
+        convert("9: 45 AM to 13 PM")
+
+def test_saiphut():
+    with pytest.raises(ValueError):
+        convert("9:61 AM to 5 PM")
+        convert("8 AM to 6:90 PM")
+
+if __name__ == "__main__":
+    main()
--- working/working.py
+++ working/working.py
@@ -8,7 +8,7 @@ def convert(s):
     if match := re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s):
         a = match.groups()
         if int(a[0]) > 12 or int(a[3]) > 12:
-            raise ValueError
+            raise ValueError 
 
         return f"{chuan_hoa(a[0], a[1], a[2])} to {chuan_hoa(a[3], a[4], a[5])}"
 

4885df2724208cc775b98834f936a91920be24c7
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 16:01:03 +0000

    Mon, Aug 28, 2023, 11:01 PM +07:00

--- um/um.py
+++ um/um.py
@@ -4,9 +4,8 @@ def main():
     print(count(input("Input: ")))
 
 def count(s):
-    a = re.findall(r"\b\W*um\W*", s, re.IGNORECASE)
-
-    return a
+    a = re.findall(r"\b(um)\b", s, re.IGNORECASE)
+    return len(a)
 
 if __name__ == "__main__":
     main()

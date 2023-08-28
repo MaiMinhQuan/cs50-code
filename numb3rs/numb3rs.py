c73326f07f6953f9aa8b4cd00e0c3885549af6fa
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 11:48:50 +0000

    Mon, Aug 28, 2023, 6:48 PM +07:00

--- numb3rs/numb3rs.py
+++ numb3rs/numb3rs.py
@@ -2,21 +2,26 @@ import re
 import sys
 
 def main():
-    #print(validate(input("IPv4 Address: ")))
-    ip = input()
-    if matches := re.search("^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip):
-        #a, b, c, d = matches.group()
-        #print(f"{a} {b} {c} {d}")
-        print(matches.group(4))
+    print(validate(input("IPv4 Address: ")))
 
-"""def validate(ip):
+def validate(ip):
     try:
         if matches := re.search("^(\d+)\.(\d+)\.(\d+)\.(\d+)$", ip):
             a = int(matches.group(1))
             b = int(matches.group(2))
             c = int(matches.group(3))
             d = int(matches.group(4))
-            if
-"""
-main()
+            if a < 0 or a > 255:
+                return False
+            if b < 0 or b > 255:
+                return False
+            if c < 0 or c > 255:
+                return False
+            if d < 0 or d > 255:
+                return False
+    except:
+        return False
+    return True
+if __name__ == "__main__":
+    main()
 

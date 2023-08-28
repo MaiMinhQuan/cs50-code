59b05e4533dd9eff14cbae8c50978bc36d1a9332
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 14:49:27 +0000

    Mon, Aug 28, 2023, 9:49 PM +07:00

--- working/working.py
+++ working/working.py
@@ -5,7 +5,7 @@ def main():
     print(convert(input("Hours: ")))
 
 def convert(s):
-    match = re.search(r"^([0-9][0-2]?:?([0-5][0-9])?) ([AP]M) to ([0-9][0-2]?:?([0-5][0-9])?) ([AP]M)$", s)
+    match = re.search(r"^([0-9][0-2]?):?([0-5][0-9])? ([AP]M) to ([0-9][0-2]?):?([0-5][0-9])? ([AP]M)$", s)
     if match:
         a = match.groups()
         return a

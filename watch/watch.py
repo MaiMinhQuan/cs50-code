620fcfba62724a275973d2baa27fed131e8a32bc
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-28 13:07:37 +0000

    Mon, Aug 28, 2023, 8:07 PM +07:00

--- watch/watch.py
+++ watch/watch.py
@@ -5,8 +5,8 @@ def main():
 
 def parse(s):
     if re.search(r"<iframe(.)*></iframe>", s):
-        if matches := re.search(r"(http(s)*://(www\.)*youtube\.com/embed/)([a-zA-Z0-9]+)></iframe>", s):
-            return "https://youtu.be/" + matches.group(1)
+        if matches := re.search(r"(http(s)*://(www\.)*youtube\.com/embed/)([a-zA-Z0-9]+)", s):
+            return "https://youtu.be/" + matches.group(4)
 
 if __name__ == "__main__":
     main():

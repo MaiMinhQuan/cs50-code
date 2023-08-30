f330a969039acc7891811d99b4c8caed5831bb25
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 04:53:06 +0000

    Wed, Aug 30, 2023, 11:53 AM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,17 +1,28 @@
 from datetime import date
 import re
-import 
+import sys
+import inflect
 chu = inflect.engine()
 
 def main():
     sn = input("Date of Birth: ")
+    ns = check_sn(sn)
+    sn = date(ns["year"], ns["month"], ns["day"])
+    hn = date.today()
+    tg = (hn - sn) * 24 * 60
+    out = chu.number_to_words(tg, andword = "")
+    print(out.capitalize() + "minutes")
+
 
 def check_sn(sn):
     if match := re.search(r"^(\d{4})-(\d{2})-(\d{2})$", sn):
         ns = {}
-        ns["year"] = match.group(1)
-        ns["month"] = match.group(2)
-        ns["day"] = match.group(3)
+        ns["year"] = int(match.group(1))
+        ns["month"] = int(match.group(2))
+        ns["day"] = int(match.group(3))
+        return ns
     else:
-        sys.
+        sys.exit("Invalid date")
 
+if __name__ == "__main__":
+    main()

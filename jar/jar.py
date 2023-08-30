064945b79bd41b4a408baaaf00dc27049332e9ef
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 06:00:44 +0000

    Wed, Aug 30, 2023, 1:00 PM +07:00

--- jar/jar.py
+++ jar/jar.py
@@ -1,23 +1,32 @@
 class Jar:
     def __init__(self, capacity = 12):
-        self.capacity = capacity
-        self.size = 0
+        if self._capacity < 0:
+            raise ValueError
+        self._capacity = capacity
+        self._size = 0
 
     def __str__(self):
         return "🍪" * self.size
 
     def deposit(self, n):
-        if n > self.capacity:
+        if n > self._capacity:
             raise ValueError
-        if self.size + n > self.capacity:
+        if self._size + n > self._capacity:
             raise ValueError
-        self.size += n
+        self._size += n
 
     def withdraw(self, n):
-        if n > self.size:
+        if n > self._size:
             raise ValueError
-        self.size -= n
+        self._size -= n
 
     @property
     def capacity(self):
-        if 
+        return self._capacity
+
+    @property
+    def size(self):
+        return self._size
+
+jar = Jar(5)
+print(jar)

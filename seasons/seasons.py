8ec6fd73fe38e68ccdff9350474a0c6862bfa8ae
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 05:00:25 +0000

    Wed, Aug 30, 2023, 12:00 PM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -11,7 +11,7 @@ def main():
     sn = date(ns["year"], ns["month"], ns["day"])
     hn = date.today()
     tg = (hn - sn) * 24 * 60
-    out = p.number_to_words(tg, andword = "")
+    out = p.number_to_words(tg)
     #print(out.capitalize() + "minutes")
 
 

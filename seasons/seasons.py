f9e79f3060b73b1e393be00ae7bcb027dc2320db
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 04:46:54 +0000

    Wed, Aug 30, 2023, 11:46 AM +07:00

--- seasons/seasons.py
+++ seasons/seasons.py
@@ -1,4 +1,17 @@
 from datetime import date
+import re
+import 
 chu = inflect.engine()
 
-def main()
+def main():
+    sn = input("Date of Birth: ")
+
+def check_sn(sn):
+    if match := re.search(r"^(\d{4})-(\d{2})-(\d{2})$", sn):
+        ns = {}
+        ns["year"] = match.group(1)
+        ns["month"] = match.group(2)
+        ns["day"] = match.group(3)
+    else:
+        sys.
+

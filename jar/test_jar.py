0ae57120e9d979c89f5a944625708cac3348c27a
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 06:13:30 +0000

    Wed, Aug 30, 2023, 1:13 PM +07:00

--- jar/test_jar.py
+++ jar/test_jar.py
@@ -11,4 +11,22 @@ def test_str():
     assert str(jar) == ""
     jar.deposit(2)
     assert str(jar) == "🍪🍪"
-    jar.deposit()
+    jar.deposit(1)
+    assert str(jar) == "🍪🍪🍪"
+    jar.withdraw(2)
+    assert str(jar) == "🍪"
+
+def test_deposit():
+    jar = Jar()
+    jar.deposit(10)
+    assert jar.size == 10
+    jar.deposit(1)
+    assert jar.size == 11
+
+def test_withdraw():
+    jar = Jar()
+    jar.deposit(10)
+    jar.withdraw(5)
+    assert jar.size == 5
+    jar.withdraw(3)
+    assert jar.size == 2

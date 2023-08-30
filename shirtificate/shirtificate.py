db8e4298e20752f6b89355b3f37902bd54387002
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-08-30 06:41:07 +0000

    Wed, Aug 30, 2023, 1:41 PM +07:00

--- shirtificate/shirtificate.py
+++ shirtificate/shirtificate.py
@@ -4,12 +4,12 @@ class PDF():
     def __init__(self, t):
         self.a = FPDF()
         self.a.add_page()
-        self.a.set_font("helvetica", "B", 50)
-        self.a.cell(0, 60, "CS50 Shirtificate", new_x = "LMARGIN", new_y = "NEXT", align = "C")
+        self.a.set_font("helvetica", "B", 45)
+        self.a.cell(0, 59, "CS50 Shirtificate", new_x = "LMARGIN", new_y = "NEXT", align = "C")
         self.a.image("shirtificate.png", w = self.a.epw)
         self.a.set_font_size(30)
         self.a.set_text_color(210, 100, 150)
-        self.a.text(x = 47.5, y = 140, txt = f"{t} took CS50")
+        self.a.text(x = 45, y = 140, txt = f"{t} took CS50")
 
     def save(self, name):
         self.a.output(name)

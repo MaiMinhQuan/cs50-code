46769fc39c0f55229fd478b4c02e8918c09a927f
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-09-07 13:45:49 +0000

    Thu, Sep 7, 2023, 8:45 PM +07:00

--- nghich/nghich.py
+++ nghich/nghich.py
@@ -1,11 +1,14 @@
 from tabulate import tabulate
 import csv
-with open("test.csv") as f:
-    a = []
-    reader = csv.reader(f)
-    for row in reader:
-        a.append(row)
+def main():
+    with open("test.csv") as f:
+        a = []
+        reader = csv.reader(f)
+        for row in reader:
+            a.append(row)
 
-print(tabulate(a[1:], headers = a[0], tablefmt = "grid"))
+    print(tabulate(a[1:], headers = a[0], tablefmt = "grid"))
+
+    
 
 

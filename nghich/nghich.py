ffcd890d633a600db99b3c0c985fda631e34e11c
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-09-07 13:15:57 +0000

    Thu, Sep 7, 2023, 8:15 PM +07:00

---
+++ nghich/nghich.py
@@ -0,0 +1,11 @@
+from tabulate import tabulate
+import csv
+with open(test.csv) as f:
+    a = []
+    reader = csv.reader(f)
+    for row in reader:
+        a.append(row)
+
+print(tabulate(a, tablefmt = "grid"))
+
+

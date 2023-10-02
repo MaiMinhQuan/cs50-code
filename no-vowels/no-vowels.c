addaf01147c0b2e55a9d43e48e5a4a452735de94
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-02 14:51:06 +0000

    Mon, Oct 2, 2023, 9:51 PM +07:00

--- no-vowels/no-vowels.c
+++ no-vowels/no-vowels.c
@@ -5,8 +5,32 @@
 
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+
+string replace(string s)
+{
+    for (int i = 0, n = strlen(s); i < n; i++)
+    {
+        if (s[i] == 'a')
+            s[i] = '6';
+        if (s[i] == 'e')
+            s[i] = '3';
+        if (s[i] == 'i')
+            s[i] = '1';
+        if (s[i] == 'o')
+            s[i] = '0';
+    }
+
+    return s;
+}
 
 int main(int argc, string argv[])
 {
-    
+    if (argc != 2)
+    {
+        printf("Usage: ./no-vowels word");
+        return 1;
+    }
+    printf("%s", replace(argv[1]));
+    return 0;
 }

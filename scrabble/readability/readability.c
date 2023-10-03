53ba8ea40bcc0bbb402af8fad04ae5d6398b05b1
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-03 02:20:23 +0000

    Tue, Oct 3, 2023, 9:20 AM +07:00

--- scrabble/readability/readability.c
+++ scrabble/readability/readability.c
@@ -1,7 +1,27 @@
 #include <cs50.h>
 #include <stdio.h>
+#include <string.h>
+#include <ctype.h>
+
+int count_letters(string text);
+int count_words(string text);
+int count_sentences(string text);
 
 int main(void)
 {
-    
+    string input = get_string("Text: ");
+    printf("%i", count_letters(input));
+    return 0;
+}
+
+int count_letters(string text)
+{
+    int length = strlen(text);
+    int cnt = 0;
+    for (int i = 0; i <length; i++)
+    {
+        if (isalpha(text[i]))
+            cnt++;
+    }
+    return cnt;
 }

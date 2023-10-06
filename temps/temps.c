0df209f622953a2e4f69abb7646d587aed36cbe7
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-06 03:23:35 +0000

    Fri, Oct 6, 2023, 10:23 AM +07:00

--- temps/temps.c
+++ temps/temps.c
@@ -62,5 +62,8 @@ int main(void)
 // TODO: Sort cities by temperature in descending order
 void sort_cities(void)
 {
-    // Add your code here
+    for (int i = 0; i < NUM_CITIES; i++)
+    {
+        for (int )
+    }
 }

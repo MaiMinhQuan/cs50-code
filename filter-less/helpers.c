889140740e5ce9d16bdaf16b77e0232df12d6d4a
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-08 04:31:09 +0000

    Sun, Oct 8, 2023, 11:31 AM +07:00

--- filter-less/helpers.c
+++ filter-less/helpers.c
@@ -36,6 +36,15 @@ void sepia(int height, int width, RGBTRIPLE image[height][width])
 // Reflect image horizontally
 void reflect(int height, int width, RGBTRIPLE image[height][width])
 {
+    for (int i = 0; i < height; i++)
+    {
+        for (int j = 0; j < width; j++)
+        {
+            RGBTRIPLE tmp = image[i][j];
+            image[i][j] = image[i][width - 1 -j];
+            image[i][width - 1 - j] = tmp;
+        }
+    }
     return;
 }
 

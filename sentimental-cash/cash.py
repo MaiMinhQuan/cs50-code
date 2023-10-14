77392acf6791c898d5307bd82748a9bc52807519
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-14 14:33:56 +0000

    Sat, Oct 14, 2023, 9:33 PM +07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -9,12 +9,14 @@ while True:
         continue
 
 n = int(n * 100)
+print(n)
 quarter = n / 25
+print(quarter)
 n -= 25 * quarter
 dime = n / 10
 n -= 10 * dime
 nickel = n / 5
 n -= 5 * nickel
 
-print(quarter + dime + nickel + n)
+#print(quarter + dime + nickel + n)
 

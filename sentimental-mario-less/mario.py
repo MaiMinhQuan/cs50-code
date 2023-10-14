2eb7739af9817daf3739a5fccf1447f5d34bf9b6
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-14 14:07:56 +0000

    Sat, Oct 14, 2023, 9:07 PM +07:00

--- sentimental-mario-less/mario.py
+++ sentimental-mario-less/mario.py
@@ -2,9 +2,10 @@ while True:
     try:
         n = int(input("Height: "))
         if n <= 0 or n > 8:
-            continue()
+            continue
+        break
     except:
-        continue()
+        continue
 
 for i in range(n):
     print(" " * (n - 1 - i), end = "")

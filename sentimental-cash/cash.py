497b19b8b65e3b4a1ad270c7ca59eba0a7a0f4be
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-14 14:32:37 +0000

    Sat, Oct 14, 2023, 9:32 PM +07:00

--- sentimental-cash/cash.py
+++ sentimental-cash/cash.py
@@ -1,3 +1,4 @@
+
 while True:
     try:
         n = float(input("Change owed: "))
@@ -8,4 +9,12 @@ while True:
         continue
 
 n = int(n * 100)
-print(n)
+quarter = n / 25
+n -= 25 * quarter
+dime = n / 10
+n -= 10 * dime
+nickel = n / 5
+n -= 5 * nickel
+
+print(quarter + dime + nickel + n)
+

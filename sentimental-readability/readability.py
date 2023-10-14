a73dde17ae74e517fc72e2caaee8feea799c6a50
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-14 14:55:43 +0000

    Sat, Oct 14, 2023, 9:55 PM +07:00

--- sentimental-readability/readability.py
+++ sentimental-readability/readability.py
@@ -4,7 +4,28 @@ def cnt(s):
     sentences = 0
     for c in s:
         if c.isalpha():
-            letters++
+            letters += 1
         if c == " ":
-            sentences++
+            words += 1
+        if c == "." or c == "?" or c == "!":
+            sentences += 1
+
+    return letters, words + 1, sentences
+
+def main():
+    s = input("Text: ")
+    l, w, s = cnt(s)
+    lw = l / w * 100
+    sw = s / w * 100
+    result = 0.0588 * lw - 0.296 * sw - 15.8
+    if result >= 16:
+        print("Grade 16+")
+    elif result < 1:
+        print("Before Grade 1")
+    else:
+        print(f"Grade {int(result)}")
+
+main()
+
+
 

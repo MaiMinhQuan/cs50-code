bfd90437a13c6acd63396a9f32bd3b5591ec3d2f
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-28 02:22:22 +0000

    Sat, Oct 28, 2023, 9:22 AM +07:00

--- prophecy/schema.sql
+++ prophecy/schema.sql
@@ -1,6 +1,22 @@
-CREAR TABLE new_students
+CREATE TABLE new_students
 (
     id INTEGER,
     student_name TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE houses
+(
+    id INTEGER,
+    house TEXT,
+    head TEXT,
+    PRIMARY KEY(id)
+);
+
+CREATE TABLE relationship
+(
+    id INTEGER,
+    student_name TEXT,
+    house TEXT,
     PRIMARY KEY(id)
 );

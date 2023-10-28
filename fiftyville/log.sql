e9e607f1d490b3276d9249c72b076eb0c4b7a03b
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-28 05:49:22 +0000

    Sat, Oct 28, 2023, 12:49 PM +07:00

--- fiftyville/log.sql
+++ fiftyville/log.sql
@@ -63,8 +63,9 @@ SET destination_airport_id = airports.id
 FROM airports
 WHERE  flights.destination_airport_id = airports.id;
 
-SELECT flights.destination_airport_id, name, phone_number, license_plate FROM people
+SELECT airports.city, flights.destination_airport_id, name, phone_number, license_plate FROM people
 JOIN passengers ON people.passport_number = passengers.passport_number
 JOIN flights ON flights.id = passengers.flight_id
+JOIN airports ON airports.id = flights.destination_airport_id
 WHERE flights.id = 36
 ORDER BY flights.hour ASC;

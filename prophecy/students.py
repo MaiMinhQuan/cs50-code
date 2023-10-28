55d223d8664fad91bc717775777418085a434056
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-28 02:14:31 +0000

    Sat, Oct 28, 2023, 9:14 AM +07:00

--- prophecy/students.py
+++ prophecy/students.py
@@ -1,16 +1,22 @@
 import csv
 
+def creat_house(house, houses, head):
+    count = 0
+    for h in houses:
+        if h["house"] == house :
+            count += 1
+    if count == 0:
+        houses.append({"house": house, "head": head})
+
+def creat_student(name, students):
+    students.append({"student_name": name})
+
+def create_relationship(name, house, relationship):
+    
 students = []
 houses = []
 relationships = []
 
-def creat_house(house, houses):
-    count = 0
-    for h in houses:
-        if h == house :
-            count += 1
-    if count == 0:
-        houses.append(house)
 
 with open("students.csv", "r") as csvfile:
     reader = csv.DictReader(csvfile)
@@ -18,4 +24,6 @@ with open("students.csv", "r") as csvfile:
         name = row["student_name"]
         house = row["house"]
         head = row["head"]
-        create_house(house, houses)
+
+        create_house(house, houses, head)
+        create_student(name, students)

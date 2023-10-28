895651fe7160728c2d369b68698e6225512f3fb3
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-10-28 04:25:39 +0000

    Sat, Oct 28, 2023, 11:25 AM +07:00

--- movies/11.sql
+++ movies/11.sql
@@ -1,3 +1,10 @@
 SELECT tile FROM movies
 JOIN ratings ON movies.id = ratings.movie_id
-JOIN stars ON ratings.
+JOIN stars ON ratings.movie_id = stars.movie_id
+WHERE stars.person_id =
+(
+    SELECT id FROM people
+    WHERE name = 'Chadwick Boseman'
+)
+ORDER BY ratings DSC
+LIMIT 5

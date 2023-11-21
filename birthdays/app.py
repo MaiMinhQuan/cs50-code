c1113ac66fc2c369c5e1481bbfa3522ebffff412
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-11-21 01:58:58 +0000

    Tue, Nov 21, 2023, 8:58 AM +07:00

--- birthdays/app.py
+++ birthdays/app.py
@@ -27,7 +27,11 @@ def index():
     if request.method == "POST":
 
         # TODO: Add the user's entry into the database
+        name = request.form.get("name")
+        month = request.form.get("month")
+        day = request.form.get("day")
 
+        db.execute("INSERT INTO birthdays (name, month, day) VALUES (?, ?, ?)", name, month, day)
         return redirect("/")
 
     else:
@@ -35,6 +39,6 @@ def index():
         # TODO: Display the entries in the database on index.html
         mn = db.execute("SELECT * FROM birthdays")
         return render_template("index.html", mn = mn)
-        
+
 
 

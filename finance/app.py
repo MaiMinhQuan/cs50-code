bd78c23ac530ec77f73708ae199f276b055b2027
MaiMinhQuan <142311790+MaiMinhQuan@users.noreply.github.com>
2023-11-22 05:55:26 +0000

    Wed, Nov 22, 2023, 12:55 PM +07:00

--- finance/app.py
+++ finance/app.py
@@ -32,13 +32,13 @@ db.execute("""
 """)
 
 db.execute("""
-    CREAT TABLE IF NOT EXITS transactions(
+    CREATE TABLE IF NOT EXISTS transactions(
            id INTERGER PRIMARY KEY AUTOINCREMENT,
            user_id INTEGER NOT NULL
            symbol TEXT NOT NULL,
            shares INTEGER NOT NULL,
            price NUMERIC NOT NULL,
-           timestamp DATETIME DEFAULT CURRENT_TIMESTAMP
+           timestamp DATETIME DEFAULT CURRENT_TIMESTAMP,
            FOREIGN KEY (user_id) REFERENCES users (id)
     )
 """)
